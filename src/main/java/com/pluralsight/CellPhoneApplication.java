aaa56d0adcf823851d31698e196414c951665ad6
TheRealRemo <timothyprice2001@gmail.com>
2026-04-14 15:30:00 -0400

    Second commit

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -4,7 +4,22 @@ import java.util.Scanner;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        CellPhone myPhone = new CellPhone();
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println(Please);
+        System.out.println("Please enter the serial number: ");
+        int serial = scanner.nextInt();
+
+        myPhone.setSerialNumber(serial);
+        //myPhone stringBuilder construction
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Serial Number: ").append(myPhone.getSerialNumber());
+
+        //final stringBuilder completion
+
+
+        System.out.println(stringBuilder);
+
     }
 }
