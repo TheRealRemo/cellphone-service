b2d93832cb76f197f32d770984489c7a1b064998
TheRealRemo <timothyprice2001@gmail.com>
2026-04-14 21:39:49 -0400

    Added CellPhoneApplication user input for serial number, model, carrier, phone number and owner name implementing CellPhone class with getters and setters.

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -4,22 +4,35 @@ import java.util.Scanner;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         CellPhone myPhone = new CellPhone();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the serial number: ");
+        System.out.print("Please enter the serial number: ");
         int serial = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Please enter the model: ");
+        String model = scanner.nextLine();
+        System.out.print("Please enter the carrier: ");
+        String carrier = scanner.nextLine();
+        System.out.print("Please enter the phone number: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.print("Please enter the owner's name: ");
+        String owner = scanner.nextLine();
+
 
         myPhone.setSerialNumber(serial);
-        //myPhone stringBuilder construction
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Serial Number: ").append(myPhone.getSerialNumber());
-
-        //final stringBuilder completion
+        myPhone.setModel(model);
+        myPhone.setCarrier(carrier);
+        myPhone.setPhoneNumber(phoneNumber);
+        myPhone.setOwner(owner);
 
 
-        System.out.println(stringBuilder);
+        System.out.println("Serial Number: " + myPhone.getSerialNumber());
+        System.out.println("Model: " + myPhone.getModel());
+        System.out.println(("Carrier: ") + myPhone.getCarrier());
+        System.out.println("Phone Number: " + myPhone.getPhoneNumber());
+        System.out.println("Owner: " + myPhone.getOwner());
+
 
     }
 }
